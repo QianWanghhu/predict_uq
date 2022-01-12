a189bca0cb2eb02a5b0a7046f186e124850e05a1
QianW <qian@hhu.edu.cn>
2022-01-12 22:45:32 +0800

    Delete the unused code for generating annual load ensemble.
    This is because we are using PEST++IES to generate observation ensemble.

--- src/load_uncertainty_estimates/visualization.py
+++ src/load_uncertainty_estimates/visualization.py
@@ -71,61 +71,6 @@ def calc_unc_min_max(fpath, fpath_fre):
         fig = plot_unc(df, height=df.DIN_base, unc_cov= unc_setting, ylabels=['DIN Load (t/yr)', 'Load ratio (Bounds/Best)'])
         plt.savefig(fpath+fig_name, format='png', dpi=300)
 
-##--------------------------------Generate observation ensembles--------------------------##
-def produce_meas_ensemble(fpath, fpath_freq, fname, n_sigma):
-    def generate_measurement_realization(upper, lower, n_sample, n_sigma):
-        """
-        The function produces the measurement realizations using the uncertainty ranges.
-        A log-normal distribution is assumed.   
-        Parameters:
-        ===========
-        upper, lower: float, the upper and lower bounds of uncertainty as percentages.
-        n_sample: int, the number of realizations to generate.
-        meas_best: float, the best measurement estimate.
-
-        Returns:
-        =========== 
-        meas_realizations: np.ndarray, of size (n_samples, 1)
-        """
-        
-        log_sigma = (np.log(upper) - np.log(lower)) / n_sigma
-        mu = 1
-        log_mu = np.log(mu)
-        lognorm_fitted = lognorm(s=log_sigma, loc=0, scale=mu)
-        norm_dist = norm(loc = log_mu, scale = log_sigma)
-        x = norm_dist.rvs(size = n_sample, random_state=612)
-        meas_realizations = np.exp(x)
-        return meas_realizations
-
-    uncertain_df = pd.read_csv(fpath+fpath_freq+fname, index_col='Year')
-    unc_bounds = uncertain_df.loc[:, 'din_cov_ratio_lower':'din_cov_ratio_upper'].values
-    meas_base = pd.read_csv(fpath+'DIN_base.csv', usecols=[3]).values
-
-    cols = [f'DIN_{year}' for year in range(2009, 2018)]
-    unc_realizations = pd.DataFrame(index=np.arange(100), columns=cols)
-    meas_realizations = pd.DataFrame(index=np.arange(100), columns=cols)
-    for ii in np.arange(9):
-        unc_realizations.loc[:, cols[ii]] = generate_measurement_realization(unc_bounds[ii, 1], \
-            unc_bounds[ii, 0], 100, n_sigma=n_sigma)
-        meas_realizations.loc[:, cols[ii]] = unc_realizations.loc[:, cols[ii]] * meas_base[ii]
-    meas_realizations.loc[:, 'pbias'] = 100 * (meas_realizations.sum(axis=1) - np.sum(meas_base)) / np.sum(meas_base)
-    meas_realizations = meas_realizations[['pbias', *cols]]
-
-    fig, axes = plt.subplots(3, 3, figsize=(6*3, 5*3))
-    k = 0
-    for ii in range(3):
-        for jj in range(3):
-            meas_realizations[cols[k]].hist(ax=axes[ii, jj])
-            axes[ii, jj].set_title(f'year {cols[k]}')
-            k += 1
-    plt.savefig(fpath+ fpath_freq + 'hist_meas_5.6_sigma.pdf', format='pdf', dpi=300)
-    meas_stats = np.round(np.array([meas_realizations.min().values, meas_realizations.max().values]).T, 2)
-    np.savetxt(fpath + fpath_fre +'meas_5.6_sigma.txt', meas_stats)
-    
-    # save the measurement realizations
-    unc_realizations.to_csv(fpath+fpath_freq+'uncertainty_realizations.csv')
-    meas_realizations.to_csv(fpath+fpath_freq+'measurement_realizations.csv')
-##--------------------------------Generate observation ensembles--------------------------##
 fpath = 'output/'
 fpath_fre = 'resample_freq70/'
-produce_meas_ensemble(fpath, fpath_fre, fname = 'uncertainty_ranges.csv', n_sigma=5.6)
+calc_unc_min_max(fpath, fpath_fre)

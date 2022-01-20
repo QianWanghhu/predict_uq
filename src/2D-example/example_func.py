1fe87b5a388ad3fdf1faf94c6b5bc7892a925756
Qian Wang <qian@hhu.edu.cn>
2022-01-20 21:33:21 +0800

    Update the settings of Model-2 in example_func.py

--- src/2D-example/example_func.py
+++ src/2D-example/example_func.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import os
 
-def rosenbrock_function(x):
+def identifiable_example(x):
     assert x.shape[0] == 2, "The first dimension of x should be 2 being equale to the number of parameters."
     # x = 4*x-2
     # vals = ((1.-x[0, :])**2+100*(x[1, :]-x[0, :]**2)**2)[:, np.newaxis]
@@ -18,9 +18,8 @@ def non_identifiable_example(x):
     assert x.shape[0], "The first dimension of x should be 2 being equale to the number of parameters."
     # x = 4 * x - 2
     # vals = ((1 - (x[0, :] + x[1, :])) ** 2 + 25 * (x[0, :] + x[1, :]) ** 2)[:, np.newaxis]
-    vals = np.zeros(shape=(2, x.shape[1]))
+    vals = np.zeros(shape=(1, x.shape[1]))
     vals[0] = 4 * x[0] - 2 + 4 * x[1] - 2
-    vals[1] = 2*(4 * x[0] - 2 + 4 * x[1] - 2) - 0.5
     return vals
 
 def wrap_function(ident):
@@ -31,7 +30,7 @@ def wrap_function(ident):
     ident: Boolean
     """
     if ident:
-        return rosenbrock_function
+        return identifiable_example
     else:
         return non_identifiable_example
 
@@ -51,23 +50,23 @@ def call_functions(x, ident=True):
     
     return vals
 
+if __name__ == '__main__':
+    output_file = 'example_output.txt'
+    print('Read Parameters')
+    parameters = pd.read_csv('parameters.csv')
+    parameters_vals = np.zeros(shape=(parameters.shape[0], 1))
+    for i,j in parameters.iterrows():
+        scaled_value = (j.upper - j.lower) * j.value/100 + j.lower 
+        parameters_vals[i] = scaled_value
 
-output_file = 'example_output.txt'
-print('Read Parameters')
-parameters = pd.read_csv('parameters.csv')
-parameters_vals = np.zeros(shape=(parameters.shape[0], 1))
-for i,j in parameters.iterrows():
-    scaled_value = (j.upper - j.lower) * j.value/100 + j.lower 
-    parameters_vals[i] = scaled_value
+    # call the function
+    vals = call_functions(parameters_vals, ident=False)
 
-# call the function
-vals = call_functions(parameters_vals, ident=False)
-
-# write outputs to output.txt
-with open(output_file, 'w') as f:
-    f.write('---- Obs 1----  \n')
-    f.write(str(vals[0]) + '\n')
-    
-    f.write('---- Obs 2 ----  \n')
-    f.write(str(vals[1]) + '\n') 
+    # write outputs to output.txt
+    with open(output_file, 'w') as f:
+        f.write('---- Obs 1----  \n')
+        f.write(str(vals[0]) + '\n')
+        
+        f.write('---- Obs 2 ----  \n')
+        f.write(str(vals[1]) + '\n') 
 
